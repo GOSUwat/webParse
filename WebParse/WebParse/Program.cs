5ae6791702c776c8e09c61071729e7007ba0a3ab
GOSUwat <78549782+GOSUwat@users.noreply.github.com>
2023-03-21 09:52:55 +0300

    smth

---
+++ WebParse/WebParse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System;
+using System.Net;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace WebParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://vk.com/turbojaba";
+            var txtPath = @"E:\webParse\WebParse\parse.txt";
+            
+
+
+
+        }
+        static HtmlDocument GetDocument(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            return doc;
+        }
+    }
+}
